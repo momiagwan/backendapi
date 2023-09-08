152f0aa6dffcc4b4a6e946446355dcde36e8aeb6
momiagwan <114867519+momiagwan@users.noreply.github.com>
2023-09-08 12:56:08 +0500

    kam  bqi hen

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const helmet=require('helmet')
-const dotenv=require('dotenv')
-const morgan=require('morgan')
- const userRoute=require('./routes/users')
- const authRoute=require('./routes/auth')
+import Express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import {router} from "../back/routes/users.js";
 
-const app=express()
-app.listen(8000,()=>{console.log("momiii")})
-dotenv.config()
-// connect function
-const connect = (uri) => {
-    return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, })
-}
-
-// conection string
-const start = async () => {
-
-    try {
-        await connect(process.env.MONGO_URI);
-        console.log("connected");
-        app.listen(8000, () => {
-            console.log("This is working", 8000);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-
-}
-// mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true},)
-// .then(()=>console.log("connected to db successfully"))
-// .catch((err)=>{console.error(err);})
-app.use(express.json())
-app.use(helmet())
-app.use(morgan("common"))
-app.get('/',(req,res)=>{res.end("welcome to homepage")})
-app.get('/users',(req,res)=>{res.end("welcome to users page")})
-app.use("/api/user",userRoute)
-app.use("/api/auth",authRoute)
-
-start()
+const app = Express();
+app.use("/api", router);
+dotenv.config();
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("connect to DB");
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+app.listen(8800, () => {
+  connect();
+  console.log("Server start");
+});
